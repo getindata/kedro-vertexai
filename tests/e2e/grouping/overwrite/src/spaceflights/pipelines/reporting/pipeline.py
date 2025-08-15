aab94226132e26dbedf899704857471a1beb3f84
Mateusz Pytel <mateusz.pytel@xebia.com>
2025-08-16 01:31:14 +0200

    feat: adjust e2e tests to new starter

---
+++ tests/e2e/grouping/overwrite/src/spaceflights/pipelines/reporting/pipeline.py
@@ -0,0 +1,36 @@
+from kedro.pipeline import Node, Pipeline
+
+from .nodes import (
+    compare_passenger_capacity_exp,
+    compare_passenger_capacity_go,
+    create_confusion_matrix,
+)
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    """This is a simple pipeline which generates a pair of plots"""
+    return Pipeline(
+        [
+            Node(
+                func=compare_passenger_capacity_exp,
+                inputs="preprocessed_shuttles",
+                outputs="shuttle_passenger_capacity_plot_exp",
+                name="compare_passenger_capacity_exp",
+                tags=["grp.preprocessing"],
+            ),
+            Node(
+                func=compare_passenger_capacity_go,
+                inputs="preprocessed_shuttles",
+                outputs="shuttle_passenger_capacity_plot_go",
+                name="compare_passenger_capacity_go",
+                tags=["grp.preprocessing"],
+            ),
+            Node(
+                func=create_confusion_matrix,
+                inputs="companies",
+                outputs="dummy_confusion_matrix",
+                name="create_confusion_matrix",
+                tags=["grp.preprocessing"],
+            ),
+        ]
+    )
